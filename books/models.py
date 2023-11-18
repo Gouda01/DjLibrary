9e6fcf342d7b58520fa383d244dfe70075b4d1c4
Gouda01 <goda61@gmail.com>
2023-11-18 21:41:51 +0200

    Make Models of Author and Book and Review

--- books/models.py
+++ books/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
+
+class Author (models.Model):
+    name = models.CharField(max_length=200)
+    birth_date = models.DateField()
+    biography = models.TextField(max_length=1000)
+
+
+class Book (models.Model):
+    title = models.CharField(max_length=100)
+    author = models.ForeignKey(Author,related_name='book_author',on_delete=models.CASCADE)
+    publication_date = models.DateField()
+    price = models.FloatField()
+
+class review (models.Model):
+    book = models.ForeignKey(Book,related_name='book_review',on_delete=models.CASCADE)
+    reviewer_name = models.CharField(max_length=200)
+    content = models.TextField(max_length=1000)
+    rating = models.IntegerField(validators=[MaxValueValidator(5), MinValueValidator(1)]
+     )
+
+
 
 # Create your models here.
